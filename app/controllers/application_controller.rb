d3bfd4d144a33d6f4505ff2367760d2c37d38d8b
Chris Hunter <chriswhunter89@gmail.com>
2022-04-11 14:41:49 +0100

    cloudinary setup and user avatar implemented and styled

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
   # For additional name fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :photo])
   end
 
   def root_path(options = {})
